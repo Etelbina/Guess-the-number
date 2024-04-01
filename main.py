a22c65c12e57eb603b975f331516d401a666ce9a
Etelbina <etelbinacanedo@gmail.com>
2024-04-01 15:27:05 -0600

    adding the random secret number

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-secret_number = 50
+import random
+
+secret_number = random.randint(1,100)
 print("Cow says: -Hi!! I'm cow.\n\t  -The system has a secret number between 1 to 100.\n\t  -Do you want to guess? Then write a number bellow\n")
 user_input = input()
 if user_input == str(secret_number):
   print(f"\nCow says: -You won!! The secret number is {secret_number}")
 elif int(user_input) > secret_number:
-  print("\nCow says: -You went too far. Now is my turn")
+  print(f"\nCow says: -You went too far. The secret number is {secret_number}")
 else:
-  print("\nCow says: -You fell short. Now is my turn")
+  print(f"\nCow says: -You fell short. The secret number is {secret_number}")
