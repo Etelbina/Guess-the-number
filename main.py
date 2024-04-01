a2753318af5c16b288f6caeb0eab8eb0a0160430
Etelbina <etelbinacanedo@gmail.com>
2024-04-01 15:13:14 -0600

    just conditionals

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print( "Hello" )
+secret_number = 50
+print("Cow says: -Hi!! I'm cow.\n\t  -The system has a secret number between 1 to 100.\n\t  -Do you want to guess? Then write a number bellow\n")
+user_input = input()
+if user_input == str(secret_number):
+  print(f"\nCow says: -You won!! The secret number is {secret_number}")
+elif int(user_input) > secret_number:
+  print("\nCow says: -You went too far. Now is my turn")
+else:
+  print("\nCow says: -You fell short. Now is my turn")
